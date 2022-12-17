bbc35ba34b292c00215b0be1986538fa16772529
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2022-12-17 23:58:38 +0100

    Add function that checks for updates of the script

--- minecraft-server-service-installer.sh
+++ minecraft-server-service-installer.sh
@@ -27,3 +27,36 @@ Show_Version_Info() {
     echo
 }
 ###########################################################################
+
+
+
+
+
+###########################################################################
+# Function that checks for a script update, download it and run it.       #
+###########################################################################
+Check_Script_Update () {
+echo "Checking for script updates..."
+mkdir -p /tmp/mitchellvanbijleveld/Minecraft-Server-Service/
+curl --output /tmp/mitchellvanbijleveld/Minecraft-Server-Service/VersionInfo https://github.mitchellvanbijleveld.dev/Minecraft-Server-Service/VERSION --silent
+. /tmp/mitchellvanbijleveld/Minecraft-Server-Service/VersionInfo
+Online_ScriptVersion=$SCRIPT
+Online_JarVersion=$JAR
+Online_JarURL=$URL
+
+if [[ $ScriptVersion < $Online_ScriptVersion ]]; then
+    ScriptName="$0"
+    echo -e "\x1B[1;33mScript not up to date ($ScriptVersion)! Downloading newest version ($Online_ScriptVersion)...\x1B[0m"
+    curl --output "./$ScriptName" https://github.mitchellvanbijleveld.dev/Minecraft-Server-Service/minecraft-server-service-installer.sh --progress-bar
+    echo
+    echo "Restarting Script..."
+    bash "./$ScriptName"
+    exit
+elif [[ $ScriptVersion > $Online_ScriptVersion ]]; then
+    echo -e "\x1B[1;33mYour version of the script ($ScriptVersion) is newer than the server version ($Online_ScriptVersion).\x1B[0m"
+else
+    echo -e "\x1B[1;32mScript is up to date.\x1B[0m"
+fi
+echo
+}
+###########################################################################
