a5761dd6be20e5d4a46536f6ed554aca49cbba8d
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-04 23:12:23 +0100

    Update minecraft-server-service-installer.sh

--- minecraft-server-service-installer.sh
+++ minecraft-server-service-installer.sh
@@ -6,16 +6,21 @@
 # \x1B[1;31m     Red / ERROR
 # \x1B[1;33m     Orange / WARNING
 
-###########################################################################
-# Script Information.                                                     #
-###########################################################################
+####################################################################################################
+# Script Information.                             ##################################################
+####################################################################################################
 ScriptName="Mitchell's Minecraft Server Service Installation Script"
 ScriptDescription="Bash script that helps installing a Minecraft Server on Linux as a system service."
 ScriptDeveloper="Mitchell van Bijleveld"
 ScriptDeveloperWebsite="https://mitchellvanbijleveld.dev/"
 ScriptVersion="2023 01 04 22 55 - beta"
 ScriptCopyright="© 2022"
-###########################################################################
+####################################################################################################
+# Mitchell van Bijleveld's Script Updater.        ##################################################
+Internal_ScriptName="Minecraft-Server-Service"
+URL_VERSION="https://github.mitchellvanbijleveld.dev/Minecraft-Server-Service/VERSION"
+URL_SCRIPT="https://github.mitchellvanbijleveld.dev/Minecraft-Server-Service/minecraft-server-service-installer.sh"
+####################################################################################################
 
 
 
@@ -25,7 +30,7 @@ ScriptCopyright="© 2022"
 # 'source' external functions from GitHub (personal dev site).            #
 ###########################################################################
 source <(curl https://github.mitchellvanbijleveld.dev/Bash-Functions/import_Functions.sh --silent)
-import_Functions echo_Verbose print_ScriptInfo
+import_Functions echo_Verbose print_ScriptInfo script_Updater
 ###########################################################################
 
 
@@ -33,34 +38,6 @@ import_Functions echo_Verbose print_ScriptInfo
 
 
 
-###########################################################################
-# Function that checks for a script update, download it and run it.       #
-###########################################################################
-Check_Script_Update () {
-echo "Checking for script updates..."
-mkdir -p /tmp/mitchellvanbijleveld/Minecraft-Server-Service/
-curl --output /tmp/mitchellvanbijleveld/Minecraft-Server-Service/VersionInfo https://github.mitchellvanbijleveld.dev/Minecraft-Server-Service/VERSION --silent
-. /tmp/mitchellvanbijleveld/Minecraft-Server-Service/VersionInfo
-Online_ScriptVersion=$SCRIPT
-Online_JarVersion=$JAR
-Online_JarURL=$URL
-
-if [[ $ScriptVersion < $Online_ScriptVersion ]]; then
-    ScriptName="$0"
-    echo -e "\x1B[1;33mScript not up to date ($ScriptVersion)! Downloading newest version ($Online_ScriptVersion)...\x1B[0m"
-    curl --output "./$ScriptName" https://github.mitchellvanbijleveld.dev/Minecraft-Server-Service/minecraft-server-service-installer.sh --progress-bar
-    echo
-    echo "Restarting Script..."
-    bash "./$ScriptName"
-    exit
-elif [[ $ScriptVersion > $Online_ScriptVersion ]]; then
-    echo -e "\x1B[1;33mYour version of the script ($ScriptVersion) is newer than the server version ($Online_ScriptVersion).\x1B[0m"
-else
-    echo -e "\x1B[1;32mScript is up to date.\x1B[0m"
-fi
-echo
-}
-###########################################################################
 
 
 
