7bc034ddf9aa1d5fef033263acfca2f88416459f
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-04 22:56:39 +0100

    Update minecraft-server-service-installer.sh

--- minecraft-server-service-installer.sh
+++ minecraft-server-service-installer.sh
@@ -13,7 +13,7 @@ ScriptName="Mitchell's Minecraft Server Service Installation Script"
 ScriptDescription="Bash script that helps installing a Minecraft Server on Linux as a system service."
 ScriptDeveloper="Mitchell van Bijleveld"
 ScriptDeveloperWebsite="https://mitchellvanbijleveld.dev/"
-ScriptVersion="2022 12 30 23 02 - beta"
+ScriptVersion="2023 01 04 22 55 - beta"
 ScriptCopyright="© 2022"
 ###########################################################################
 
