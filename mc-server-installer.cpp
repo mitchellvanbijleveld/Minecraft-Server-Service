47397bdaf204e350f497157ad5388b0340bc4612
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-06-07 21:14:34 +0200

    Remove temp script after running

--- mc-server-installer.cpp
+++ mc-server-installer.cpp
@@ -8,12 +8,15 @@ int main(int argc, char* argv[]) {
   std::system(download_command.c_str());
 
   std::string run_command = "bash script.sh";
+  std::string run_command_original = "script.sh";
 
   for (int i = 1; i < argc; i++) {
     run_command += " " + std::string(argv[i]);
   }
-
   std::system(run_command.c_str());
+  
+  std::string rm_command = "rm -rf script.sh";
+  std::system(rm_command.c_str());
 
   return 0;
 }
